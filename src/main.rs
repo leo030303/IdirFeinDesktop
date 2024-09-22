67966c8c19506abb23c685e149e397cae02eaa35
Leo Ring <leoring03@gmail.com>
2024-09-22 15:44:08 +0100

    Default to fullscreen window size

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use iced::Size;
 use idirfein_desktop_iced::app::AppState;
 
 pub fn main() -> iced::Result {
     iced::application("IdirFéin Desktop", AppState::update, AppState::view)
         .theme(AppState::theme)
         .exit_on_close_request(false)
+        .window_size(Size::INFINITY)
         .subscription(AppState::subscription)
         .run_with(AppState::new)
 }
