c9be8e9fc0fce358d13bdbe6a3ea057caa711468
Leo Ring <leoring03@gmail.com>
2024-11-03 23:42:38 +0000

    Switched font to Gnomes font, which looks nicer

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub fn main() -> iced::Result {
     iced::application("IdirFéin Desktop", AppState::update, AppState::view)
         .theme(AppState::theme)
         .exit_on_close_request(false)
+        .default_font(iced::Font::with_name("Cantarell"))
         .window_size(Size::INFINITY)
         .subscription(AppState::subscription)
         .run_with(AppState::new)
